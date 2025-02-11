7013839267fa1d13e5682e244ec8784627f99cf0
MarwanabdalR <marwanabdalrady13@gmail.com>
2025-02-11 22:27:41 +0200

    Directors of projects

--- E-ProvaUI/src/App.jsx
+++ E-ProvaUI/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+import "./Pages/Auth/style.css"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1 className="bg-red-500 font-sans underline">hello!</h1>
+      <i className="fa-solid fa-envelope fa-2xl red-color"></i>
     </>
   )
 }
