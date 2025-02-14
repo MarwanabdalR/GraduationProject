9a30eb49774e1759f89af6d141346512d5b141bd
MariamElsayed172 <105307571+MariamElsayed172@users.noreply.github.com>
2025-02-14 15:58:07 +0200

    login style
    without the logic to deal with data or APIs

--- E-ProvaUI/src/App.jsx
+++ E-ProvaUI/src/App.jsx
@@ -1,11 +1,10 @@
 import "./Pages/Auth/style.css"
-
+import Login from "./Pages/Auth/Login"
 function App() {
 
   return (
     <>
-      <h1 className="bg-red-500 font-sans underline">hello!</h1>
-      <i className="fa-solid fa-envelope fa-2xl red-color"></i>
+    <Login />
     </>
   )
 }
