38d8a4a10ea1e2a260eed872912f00fa9f8c1ab1
MarwanabdalR <marwanabdalrady13@gmail.com>
2025-02-16 01:16:37 +0200

    essential adds
    add meta data and ico
    add browsing route and layout
    updates on login styles

--- E-ProvaUI/src/App.jsx
+++ E-ProvaUI/src/App.jsx
@@ -1,11 +1,27 @@
 import "./Pages/Auth/style.css"
 import Login from "./Pages/Auth/Login"
+import { createBrowserRouter, RouterProvider } from "react-router"
+import Layout from "./Pages/Layout/Layout"
 function App() {
-
+  const route = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Layout />,
+        children: [
+          {
+            path: "/",
+            element: <Login />
+          }
+        ]
+          
+      }
+    ]
+  )
   return (
-    <>
-    <Login />
-    </>
+    <div className="px-6">
+      <RouterProvider router={route} />
+    </div>
   )
 }
 
--- E-ProvaUI/src/Pages/Auth/Login.jsx
+++ E-ProvaUI/src/Pages/Auth/Login.jsx
@@ -1,52 +1,104 @@
-import React from 'react'
+import { useState } from "react";
 
 
 export default function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+
+
   return (
-    <main className='mb-32 block'>
-      <div className='overflow-hidden block'>
-        <section className='relative bg-no-repeat bg-cover block bg-white py-[clamp(0px,160px,35px)] mb-[clamp(0px,0px,40px)] border-t-1'>
-          <div className='relative w-full pr-3.5 pl-3.5 my-auto block'>
+    <main className="mb-32 block">
+      <div className="overflow-hidden block">
+        <section className="relative bg-no-repeat bg-cover block bg-white py-[clamp(0px,160px,35px)] mb-[clamp(0px,0px,40px)] border-t-1">
+          <div className="relative w-full pr-3.5 pl-3.5 my-32 block">
             {/*font-family should be "DM Sans", sans-serif*/}
-            <h1 className=' text-center font-sans font-semibold text-2xl  text-[#181818] mb-1 relative leading-normal block'>ACCOUNT</h1>
-            <ol className='justify-center bg-white m-0 p-0 relative z-0 flex flex-wrap list-none rounded'>
-              <li className='mr-3.5 pr-2.5 relative  before:w-1 before:h-1 before:rotate-45 before:absolute before:-right-1 before:top-1/2 before:rounded-sm before:bg-[#222]'>
-                <a href="#" className=' text-xs font-medium duration-0 touch-manipulation text-[#222]'>Home</a>
+            <h1 className=" text-center font-sans font-bold text-2xl  text-[#181818] mb-1 relative leading-normal block">
+              ACCOUNT
+            </h1>
+            <ol className="justify-center bg-white m-0 p-0 relative z-0 flex flex-wrap list-none rounded">
+              <li className="mr-3.5 pr-2.5 relative  before:w-1 before:h-1 before:rotate-45 before:absolute before:-right-1 before:top-1/2 before:rounded-sm before:bg-[#222]">
+                <a
+                  href="#"
+                  className=" text-xs font-medium duration-0 touch-manipulation text-[#222]"
+                >
+                  Home
+                </a>
               </li>
-              <li className="inline-block  ">
-                <span className=' text-xs font-medium text-[#222]'>Account</span>
+              <li className="inline-block">
+                <span className=" text-xs font-medium text-[#222]">
+                  Account
+                </span>
               </li>
             </ol>
           </div>
         </section>
-        <div className='pb-[30px] w-full px-[15px] mx-auto block'>
-          <div className='justify-center flex flex-wrap -mx-[15px]'>
-            <div className=' basis-1/2 w-full px-[15px]'>
-              <form action="/account/login" className='block mt-0'>
-                <h1 className='font-sans font-semibold text-2xl text-[#181818]' >SIGN IN</h1>
-                <p className='text-xs font-normal mb-[15px] text-[#181818]' >Insert your account information:</p>
-                <div className='mb-[15px]'>
-                  <input className='border outline-none border-gray-200 hover:border-[#e94328] px-5 py-0 h-[50px] w-full mb-0 rounded-3xl font-sans text-xs font-medium inline-block max-w-full leading-5 overflow-x-hidden overflow-y-auto' type="text" placeholder='ENTER YOUR EMAIL' required />
+        <div className="pb-[30px] w-full px-[15px] mx-auto block">
+          <div className="justify-center flex flex-wrap -mx-[15px]">
+            <div className=" basis-1/2 w-full px-[15px]">
+              <form action="/account/login" className="block mt-0">
+                <h1 className="font-sans font-bold text-2xl text-[#181818]">
+                  SIGN IN
+                </h1>
+                <p className="text-xs font-normal mb-[15px] text-[#181818]">
+                  Insert your account information:
+                </p>
+                <div className="mb-[15px]">
+                  <input
+                    className="border outline-none border-gray-200 hover:border-[#e94328] px-5 py-0 h-[50px] w-full mb-0 rounded-3xl font-sans text-xs font-medium inline-block max-w-full leading-5 overflow-x-hidden overflow-y-auto"
+                    type="text"
+                    placeholder="ENTER YOUR EMAIL"
+                    required
+                  />
                 </div>
-                <div className='relative mb-[15px]'>
-                  <input className='border outline-none border-gray-200  hover:border-[#e94328] px-5 py-0 h-[50px] w-full mb-0 rounded-3xl font-sans text-xs font-medium inline-block max-w-full leading-5 overflow-x-hidden overflow-y-auto' type="password" placeholder='PASSWORD' required />
-                  <div className='block absolute bottom-px right-px px-4 py-0.5 leading-10 cursor-pointer'>
-                    <i className="fa-solid fa-eye fa-xs text-[#7c7e83] hover:text-black"></i>
+                <div className="relative mb-[15px]">
+                  <input
+                    className="border outline-none border-gray-200  hover:border-[#e94328] px-5 py-0 h-[50px] w-full mb-0 rounded-3xl font-sans text-xs font-medium inline-block max-w-full leading-5 overflow-x-hidden overflow-y-auto"
+                    type="password"
+                    placeholder="PASSWORD"
+                    required
+                  />
+                  <div
+                    className="block absolute bottom-px right-px px-4 py-0.5 leading-10 cursor-pointer"
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {" "}
+                    <i
+                      className={`fa-solid ${
+                        showPassword ? "fa-eye-slash" : "fa-eye"
+                      } fa-xs text-[#7c7e83] hover:text-black`}
+                    ></i>
                   </div>
                 </div>
-                <div className='flex items-center mt-5 mb-[15px] font-medium'>
+                <div className="flex items-center mt-5 mb-[15px] font-medium">
                   <i className="mr-2 mt-1 text-base inline-block fa-solid fa-envelope fa-sm text-[#e94328]"></i>
-                  <a href="#" className='text-xs text-[#181818] duration-0'>Forgot your <strong className='font-bold hover:text-[#e94328]'>Password ?</strong></a>
+                  <a href="#" className="text-xs text-[#181818] duration-0">
+                    Forgot your{" "}
+                    <strong className="font-bold hover:text-[#e94328]">
+                      Password ?
+                    </strong>
+                  </a>
                 </div>
-                <div className='font-sans font-normal text-xs mx-0 mt-3 mb-5'>
-                  <p>If you don't have an account, please <a href="" className='font-sans font-bold text-xs text-[#6aa1da] hover:text-[#e94328] duration-0'>Register Here</a></p>
+                <div className="font-sans font-normal text-xs mx-0 mt-3 mb-5">
+                  <p>
+                    If you do not have an account, please{" "}
+                    <a
+                      href=""
+                      className="font-sans font-regular text-xs text-[#6aa1da] hover:text-[#e94328] duration-0"
+                    >
+                      Register Here
+                    </a>
+                  </p>
                 </div>
-                <button type="submit" className='w-full min-w-[clamp(60px,140px,140px)] h-10 px-5 py-0 text-white bg-[#181818] hover:bg-[#0b0b0b] font-sans rounded-3xl font-semibold text-xs text-center'>LOGIN</button>
+                <button
+                  type="submit"
+                  className="w-full min-w-[clamp(60px,140px,140px)] h-10 px-5 py-0 text-white bg-[#181818] hover:bg-[#0b0b0b] font-sans rounded-3xl font-semibold text-xs text-center"
+                >
+                  LOGIN
+                </button>
               </form>
             </div>
           </div>
         </div>
       </div>
     </main>
-  )
+  );
 }
