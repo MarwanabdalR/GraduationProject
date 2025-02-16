8d744e4e42e6c5fb8722907e3e7b3da7c472ac3b
MarwanabdalR <marwanabdalrady13@gmail.com>
2025-02-16 06:19:30 +0200

    forgot password
    header for auth
    forgot password

--- E-ProvaUI/src/Pages/Auth/ForgetPassword.jsx
+++ E-ProvaUI/src/Pages/Auth/ForgetPassword.jsx
@@ -1,8 +1,51 @@
+import { Link } from "react-router";
+import HedearSection from "./HedearSection";
 
 export default function ForgetPassword() {
   return (
     <div>
-      forget Password
+      <HedearSection />
+      <div className="pb-[30px] w-full min-w-full px-[15px] block">
+        <div className="justify-center flex flex-wrap -mx-[15px]">
+          <div className="lg:w-1/2 lg:mx-auto sm:w-full justify-center flex flex-wrap -mx-[15px]">
+            <form action="/account/login" className="w-96 lg:w-full">
+              <h1 className="font-sans font-semibold text-xl md:text-3xl text-[#181818] lg:w-full">
+                Reset your password
+              </h1>
+              <hr className="my-5 border-t-2 border-[#8486e2] w-1/4" />
+              <div className="mb-[15px] sm:w-11/12 sm:mx-auto">
+                <p className="text-xs font-normal mb-[15px] text-gray-500">
+                  We will send you an email to reset your password.
+                </p>
+                <input
+                  className="border outline-none border-gray-200 hover:border-[#e94328] px-5 py-0 h-[50px] w-full mb-0 rounded-3xl font-sans text-xs font-medium inline-block max-w-full leading-5 overflow-x-hidden overflow-y-auto"
+                  type="text"
+                  placeholder="ENTER YOUR EMAIL"
+                  required
+                />
+              </div>
+              <div className="flex justify-start gap-5 uppercase">
+                <Link>
+                  <button
+                    type="submit"
+                    className="px-8 py-3 text-white bg-[#181818] hover:bg-[#0b0b0b] font-sans rounded-3xl text-xs font-medium text-center"
+                  >
+                    SUBMIT
+                  </button>
+                </Link>
+                <Link to="/e-prova/login">
+                  <button
+                    type="submit"
+                    className="px-8 py-3 text-white bg-[#181818] hover:bg-[#0b0b0b] font-sans rounded-3xl text-xs font-medium text-center"
+                  >
+                    CANCEL
+                  </button>
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- E-ProvaUI/src/Pages/Auth/Login.jsx
+++ E-ProvaUI/src/Pages/Auth/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router";
+import HedearSection from "./HedearSection";
 
 export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
@@ -7,27 +8,7 @@ export default function Login() {
   return (
     <main className="my-10">
       <div className="overflow-hidden block">
-        <section className="relative mb-10 lg:mb-52">
-          <div className="relative block">
-            {/*font-family should be "DM Sans", sans-serif*/}
-            <h1 className=" text-center font-sans font-semibold text-2xl  text-[#181818] mb-1 relative leading-normal block md:text-xl">
-              ACCOUNT
-            </h1>
-            <ol className="justify-center bg-white m-0 p-0 relative z-0 flex flex-wrap list-none rounded">
-              <li className="mr-3.5 pr-2.5 relative  before:w-1 before:h-1 before:rotate-45 before:absolute before:-right-1 before:top-1/2 before:rounded-sm before:bg-[#222]">
-                <Link
-                  to="/e-prova/home"
-                  className=" text-xs font-light duration-0 touch-manipulation text-[#222]"
-                >
-                  Home
-                </Link>
-              </li>
-              <li className="inline-block">
-                <span className=" text-xs font-light text-[#222]">Account</span>
-              </li>
-            </ol>
-          </div>
-        </section>
+      <HedearSection />
         <div className="pb-[30px] w-full min-w-full px-[15px] block">
           <div className="justify-center flex flex-wrap -mx-[15px]">
             <div className="lg:w-1/2 lg:mx-auto sm:w-full justify-center flex flex-wrap -mx-[15px]">
