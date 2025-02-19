e19191cd7f16319e76d18ba558164e3611045d07
MariamElsayed172 <105307571+MariamElsayed172@users.noreply.github.com>
2025-02-20 01:35:17 +0200

    bottom bar

--- E-ProvaUI/src/Pages/Layout/Layout.jsx
+++ E-ProvaUI/src/Pages/Layout/Layout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router";
 import Nav from "./Nav";
 import Footer from "./Footer";
 import BackToTopButton from "./BackToTopButton";
+import BottomMenu from "./BottomMenu";
 
 export default function Layout() {
   return (
@@ -10,7 +11,9 @@ export default function Layout() {
       <div className="mx-3 mt-32">
         <Outlet />
         <BackToTopButton />
+        
       </div>
+      <BottomMenu />
       <Footer />
     </>
   );
